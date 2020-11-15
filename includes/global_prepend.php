9dd28c8474f1a50446263dd5baa1827485d7642c
mcmayn <cybehad@gmail.com>
2020-11-16 02:15:43 +0300

    added .htaccess file, mc_page_md table to database.

---
+++ includes/global_prepend.php
@@ -0,0 +1,24 @@
+<?php
+function callback($b){
+    $mobile = "m.blogmcmay.com";
+    $web_domain = "www.blogmcmay.com";
+    if ($_SERVER['SERVER_NAME'] == $mobile){
+        //replace www.... with m....
+        $b = str_replace($web_domain, $mobile, $b);
+        // replace all hyperlinked images with regular links, using the alt text
+        $b = preg_replace('/(<a[^>]*>)(<img[^>]+alt=")([^"]*)("[^>]*>)(<\/a>)/i' ,'<p class="link">$1$3$5</p>' , $b);
+        // replace images with paragraph tags
+        $b = preg_replace('/(<img[^>]+alt=")([^"]*)("[^>]*>)/' , '<p class="image">[$2]</p>' , $b);
+        // strip out stylesheet calls
+        $b = preg_replace('/(<link[^>]+rel="[^"]*stylesheet"[^>]*>|style="[^"]*")/i' , '' , $b);
+        //remove scripts
+        $b = preg_replace('/<script[^>]*>.*?<\/script>/i' , '' , $b);
+        // remove style tags and comments
+        $b = preg_replace('/<style[^>]*>.*?<\/style>|<!--.*?-->/i' , '' , $b);
+        // add robots nofollow directive to keep the search engines out!
+        $b = preg_replace('/<\/head>/i' , '<meta name="robots" content="noindex, nofollow"></head>' , $b);
+
+    }
+    return $b;
+}
+ob_start("callback");
--- style.js
+++ style.js
@@ -1,3 +1,18 @@
 function hide_lists() {
 
 }
+
+//Cross fade
+images = [
+    "images/pasta.jpg" ,
+    "images/tacosalad.jpg" ,
+    "images/phadthai.jpg" ,
+    "images/chickenmac.jpg"
+]
+var image_box = $("#main_image" );
+image_box.css({'position' : 'relative' , 'height' : '144px' } );
+var image_html = "" ;
+for(var i = 0; i < images.length; i++) {
+    image_html += '<img style="position:absolute;top:0; z-index:' + (images.length - i) + '" src="' + images[i] + '" id="image_' + i + '"/>' ;
+};
+image_box.html(image_html);
